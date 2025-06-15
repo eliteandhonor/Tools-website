664add797a616fac9f1ef5d2e3a1bdc610fcef89
eliteandhonor <132749715+eliteandhonor@users.noreply.github.com>
2025-06-15 14:57:11 +1000

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./tools/**/*.html",
+    "./scripts/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
